d10d00106d4f78ec6c219a07e9ae01bbd1383d81
Maria Luiza Lima <132663650+mlrlima@users.noreply.github.com>
2024-12-06 15:11:58 -0300

    day5

---
+++ 2024/day5pt1.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<string>
+#include<map>
+#include<vector>
+using namespace std;
+
+bool search(string p, vector<string>&rule){
+    for(int i=0;i<rule.size();i++){
+        if(rule[i]==p){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+
+    string s;
+
+    map<string, vector<string> > rules;
+    while(getline(cin,s)){
+        if(s==""){
+            break;
+        }
+
+        string page1="";page1+=s[0];page1+=s[1];
+        string page2="";page2+=s[3];page2+=s[4];
+
+        rules[page1].push_back(page2);
+    }
+
+    int ans=0;
+    while(getline(cin,s)){
+        int i=0;
+        string aux="";
+        vector<string> order;
+        while(i<s.size()){
+            aux+=s[i];
+            i++;
+            if(s[i]==',' || i==s.size()){
+                order.push_back(aux);
+                aux="";
+                i++;
+                continue;
+            }
+        }
+
+
+        bool validOrder=true;
+        for(i=order.size()-1;i>=0;i--){
+            for(int j=i-1;j>=0;j--){
+                if(!search(order[j], rules[order[i]])){
+                    validOrder=false;
+                    break;
+                }
+            }
+            if(!validOrder){
+                break;
+            }
+        }
+
+        if(validOrder){
+            int mid=order.size()/2;
+            ans+=stoi(order[mid]);
+        }
+    }
+
+    cout<<ans<<endl;
+
+    return 0;
+}
